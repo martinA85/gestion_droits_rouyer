8ce3575da073325ff2d01bb087b485e78139fa46
Martin Allimonier <allimonier.martin@gmail.com>
2018-08-13 09:31:56 +0200

    ajout de commentaire

--- models/res_users.py
+++ models/res_users.py
@@ -1,7 +1,7 @@
 from odoo import api, fields, models
 
-
+# Héritage du model user pour ajouter une liste de marque autoriser
 class ResUsers(models.Model):
     _inherit = 'res.users'
-
+    
     marque_ids = fields.Many2many('brand_model.marque', string="Modele autoriser pour l'utilisateur")
