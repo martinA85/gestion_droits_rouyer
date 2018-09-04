2aed3b9ed393bd3883662a305d071394715c6724
Dabgrall <abgrall.damien@gmail.com>
2018-09-04 12:22:18 +0200

    fusion

--- __manifest__.py
+++ __manifest__.py
@@ -19,7 +19,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','brand_model_management'],
+    'depends': ['base','rouyer_gestion_vehicule'],
 
     # always loaded
     'data': [
